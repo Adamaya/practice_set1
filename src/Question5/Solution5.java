226f77638b9f65c02d6ee5913da6eb9a703cef41
Adamaya <mail.adamyas@gmail.com>
2019-08-21 22:02:34 +0530

    variable and input object declared

--- src/Question5/Solution5.java
+++ src/Question5/Solution5.java
@@ -1,4 +1,13 @@
 package Question5;
 
+import java.util.Scanner;
+
 public class Solution5 {
+    public static void main(String[] args) {
+        int num1;
+        Scanner input = new Scanner(System.in);
+        num1 = input.nextInt();
+        input.close();
+
+    }
 }
