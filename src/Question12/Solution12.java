900534603ef965ac4d1fb901635757cbce007274
Adamaya <mail.adamyas@gmail.com>
2019-08-21 23:31:56 +0530

    ques5 replaced with ques 12 and new ques 5 created

---
+++ src/Question12/Solution12.java
@@ -0,0 +1,21 @@
+package Question12;
+
+import java.util.Scanner;
+
+public class Solution12 {
+    public static void main(String[] args) {
+        int num1;
+        Scanner input = new Scanner(System.in);
+        num1 = input.nextInt();
+        input.close();
+        for (int i = 2; i < num1; i++) {
+            if (num1 % i == 0) {
+                System.out.println("not Prime number");
+                break;
+            } else {
+                System.out.println("Prime number");
+                break;
+            }
+        }
+    }
+}
