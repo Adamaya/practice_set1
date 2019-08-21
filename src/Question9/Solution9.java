dcf862862ae9954a2e0ac837d1c469e5f8a67877
Adamaya <mail.adamyas@gmail.com>
2019-08-21 22:54:04 +0530

    logic added

--- src/Question9/Solution9.java
+++ src/Question9/Solution9.java
@@ -6,6 +6,11 @@ public class Solution9 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
+        if (name.equals(name.toLowerCase())) {
+            System.out.println(name.toUpperCase());
+        } else {
+            System.out.println(name.toLowerCase());
+        }
 
     }
 }
