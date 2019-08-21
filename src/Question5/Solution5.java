8a56827d96c9b587060682cc7bf50f7edf36e7f0
Adamaya <mail.adamyas@gmail.com>
2019-08-21 17:58:21 +0530

    input char added

--- src/Question5/Solution5.java
+++ src/Question5/Solution5.java
@@ -1,7 +1,13 @@
 package Question5;
 
+import java.util.Scanner;
+
 public class Solution5 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char char1 = input.nextLine().charAt(0);
+        char char2 = input.nextLine().charAt(0);
+        input.close();
 
     }
 }
