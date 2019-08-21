efb03a4c59d9a914ac164aff539d0ef2cd43d982
Adamaya <mail.adamyas@gmail.com>
2019-08-21 17:37:39 +0530

    Input code added

--- src/Question3/Solution3.java
+++ src/Question3/Solution3.java
@@ -2,4 +2,8 @@ package Question3;
 import java.util.Scanner;
 
 public class Solution3 {
+    Scanner input= new Scanner(System.in);
+    int num1=input.nextInt();
+    int num2=input.nextInt();
+    input.close();
 }
