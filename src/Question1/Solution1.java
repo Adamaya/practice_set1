9ff01c90889847250533ae9001dac8a34a07f3bf
Adamaya <mail.adamyas@gmail.com>
2019-08-21 17:27:39 +0530

    solution 1 is added

--- src/Question1/Solution1.java
+++ src/Question1/Solution1.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 public class Solution1 {
     public static void main(String[] args) {
-
+        Scanner input= new Scanner(System.in);
+        String name=input.nextLine();
+        input.close();
+        System.out.println("Welcome, "+name+"!");
     }
 }
