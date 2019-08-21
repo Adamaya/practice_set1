2e50c2214b5c373f6143930af691c8e04ef2f15e
Adamaya <mail.adamyas@gmail.com>
2019-08-21 23:44:45 +0530

    input object created

--- src/Question13/Solution13.java
+++ src/Question13/Solution13.java
@@ -1,4 +1,12 @@
 package Question13;
 
+import java.util.Scanner;
+
 public class Solution13 {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+
+    }
 }
