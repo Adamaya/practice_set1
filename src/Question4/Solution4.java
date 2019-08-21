4584336eb09eb5d11e9af505113dfc722e3275b9
Adamaya <mail.adamyas@gmail.com>
2019-08-21 17:43:17 +0530

    input object added

--- src/Question4/Solution4.java
+++ src/Question4/Solution4.java
@@ -1,7 +1,11 @@
 package Question4;
 
+import java.util.Scanner;
+
 public class Solution4 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        input.close();
     }
 }
