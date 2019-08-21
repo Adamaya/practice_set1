7997586a975cd65143e0f3dde70e6e5a31301905
Adamaya <mail.adamyas@gmail.com>
2019-08-21 17:34:36 +0530

    solution 2 is added

--- src/Question2/Solution2.java
+++ src/Question2/Solution2.java
@@ -2,4 +2,11 @@ package Question2;
 import java.util.*;
 
 public class Solution2 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        String name=input.nextLine();
+        input.close();
+        String[] list=name.split(" ",5);
+        System.out.println(list[0]+" University "+list[1]);
+    }
 }
