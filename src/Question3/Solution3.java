09015b1aea8a52d30bb57afa6f3fbe67b17a9306
Adamaya <mail.adamyas@gmail.com>
2019-08-21 17:39:49 +0530

    psvm is added

--- src/Question3/Solution3.java
+++ src/Question3/Solution3.java
@@ -2,13 +2,19 @@ package Question3;
 import java.util.Scanner;
 
 public class Solution3 {
-    Scanner input= new Scanner(System.in);
-    int num1=input.nextInt();
-    int num2=input.nextInt();
-    input.close();
-    int sum = num1 + num2;
-    int sub = num1 - num2;
-    int mul = num1 * num2;
-    int dev = num1 / num2;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        input.close();
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        int mul = num1 * num2;
+        int dev = num1 / num2;
+        System.out.println(num1 + "+" + num2 + "=" + sum);
+        System.out.println(num1 + "-" + num2 + "=" + sub);
+        System.out.println(num1 + "*" + num2 + "=" + mul);
+        System.out.println(num1 + "/" + num2 + "=" + dev);
+    }
 
 }
