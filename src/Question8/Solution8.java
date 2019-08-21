f037d14199a4487e9ac5aa06c78609889aaa760f
Adamaya <mail.adamyas@gmail.com>
2019-08-21 22:45:21 +0530

    correction in comparision syntax

--- src/Question8/Solution8.java
+++ src/Question8/Solution8.java
@@ -7,13 +7,13 @@ public class Solution8 {
         Scanner input = new Scanner(System.in);
         String gender = input.nextLine();
         int age = input.nextInt();
-        if (gender == "female" && age >= 1 && age <= 58) {
+        if (gender.equals("female") && age >= 1 && age <= 58) {
             System.out.println("8.2%");
-        } else if (gender == "female" && age > 58 && age <= 100) {
+        } else if (gender.equals("female") && age > 58 && age <= 100) {
             System.out.println("9.2%");
-        } else if (gender == "male" && age >= 1 && age <= 58) {
+        } else if (gender.equals("male") && age >= 1 && age <= 58) {
             System.out.println("8.4%");
-        } else if (gender == "male" && age > 58 && age <= 100) {
+        } else if (gender.equals("male") && age > 58 && age <= 100) {
             System.out.println("10.5%");
         } else {
             System.out.println("Invailid Input");
