764f3baacb46dafce0c600fad3b5e400960ee7a2
Adamaya <mail.adamyas@gmail.com>
2019-08-21 22:19:06 +0530

    input object added

--- src/Question7/Solution7.java
+++ src/Question7/Solution7.java
@@ -1,4 +1,12 @@
 package Question7;
 
+import java.util.Scanner;
+
 public class Solution7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char data = input.next().charAt(0);
+        input.close();
+
+    }
 }
