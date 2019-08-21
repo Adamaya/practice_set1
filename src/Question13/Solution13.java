a5dc26b154963f18ed4cedd3ac7245d3ff0a3749
Adamaya <mail.adamyas@gmail.com>
2019-08-21 23:45:06 +0530

    logic added

--- src/Question13/Solution13.java
+++ src/Question13/Solution13.java
@@ -7,6 +7,11 @@ public class Solution13 {
         int sum = 0;
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
-
+        while (num > 0) {
+            int remainder = num % 10;
+            sum += remainder;
+            num = num / 10;
+        }
+        System.out.println(sum);
     }
 }
