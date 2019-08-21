d5037d69fd6fda844fe581280105122314407230
Adamaya <mail.adamyas@gmail.com>
2019-08-21 22:28:54 +0530

    logic added

--- src/Question7/Solution7.java
+++ src/Question7/Solution7.java
@@ -7,6 +7,15 @@ public class Solution7 {
         Scanner input = new Scanner(System.in);
         char data = input.next().charAt(0);
         input.close();
+        int ascii = data;
+        System.out.println(ascii);
+        if (ascii >= 97 && ascii <= 122 || ascii >= 65 && ascii <= 90) {
+            System.out.println("Alphabet");
+        } else if (ascii >= 48 && ascii <= 57) {
+            System.out.println("Digit");
+        } else if (ascii >= 33 && ascii <= 47) {
+            System.out.println("Special Character");
+        }
 
     }
 }
