2a721fb48bbe4b131c531b13d1991650ec4f465f
Adamaya <mail.adamyas@gmail.com>
2019-08-21 22:42:34 +0530

    input object created

--- src/Question8/Solution8.java
+++ src/Question8/Solution8.java
@@ -1,4 +1,12 @@
 package Question8;
 
+import java.util.Scanner;
+
 public class Solution8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String gender = input.nextLine();
+        int age = input.nextInt();
+
+    }
 }
