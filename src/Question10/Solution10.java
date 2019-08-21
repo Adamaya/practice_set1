27622fb02fbffa9a3027ba31e9bff79be8880be0
Adamaya <mail.adamyas@gmail.com>
2019-08-21 23:17:28 +0530

    scanner object created

--- src/Question10/Solution10.java
+++ src/Question10/Solution10.java
@@ -1,7 +1,10 @@
 package Question10;
 
+import java.util.Scanner;
+
 public class Solution10 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
     }
 }
