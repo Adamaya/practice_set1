f268d8df29020cf4fd5cd51b49173547a946f6da
Adamaya <mail.adamyas@gmail.com>
2019-08-21 23:34:48 +0530

    odd even logic added in ques 5

--- src/Question5/Solution5.java
+++ src/Question5/Solution5.java
@@ -1,4 +1,16 @@
 package Question5;
 
+import java.util.Scanner;
+
 public class Solution5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        input.close();
+        if (num % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
 }
