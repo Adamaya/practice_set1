e8f16a87e32c64a51fa230c51001e498e6e5de27
Adamaya <mail.adamyas@gmail.com>
2019-08-21 22:53:43 +0530

    environment configured and Scanner object created

--- src/Question9/Solution9.java
+++ src/Question9/Solution9.java
@@ -1,4 +1,11 @@
 package Question9;
 
+import java.util.Scanner;
+
 public class Solution9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+
+    }
 }
