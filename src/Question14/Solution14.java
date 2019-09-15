5126000b2a8bdbb39891c4a2c9dfe4d681822e86
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-15 17:49:10 +0530

    solution 14 completed

--- src/Question14/Solution14.java
+++ src/Question14/Solution14.java
@@ -1,4 +1,21 @@
 package Question14;
 
+import java.util.Scanner;
+
+
+
 public class Solution14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String num = input.nextLine();
+        input.close();
+//        int i=0;
+//        while(i<length(num)
+        String num1 = "";
+        for (int i = num.length(); i < 0; i--) {
+            num1 += num[i];
+        }
+        System.out.println(num1);
+
+    }
 }
