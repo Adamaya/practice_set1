9bf21865c0c04522b1d1c7c1178f6f2d85930a69
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-16 20:45:27 +0530

    object 1 deleted and some change in object2

--- src/inheritance/ObjectConstructor.java
+++ src/inheritance/ObjectConstructor.java
@@ -1,25 +1,35 @@
 package inheritance;
 
-class Object1 {
-    double h = 10;
-    double d = 10;
-    double g = 10;
-}
+//class Object1 {
+//    double h = 10;
+//    double d = 10;
+//    double g = 10;
+//}
 
-class Object2 extends Object1 {
+class Object2 {
     double height;
     double density;
     double gravity;
 
     public Object2(Object2 ob) {
-        height = ob.h;
-        gravity = ob.g;
-        density = ob.d;
+        height = ob.height;
+        gravity = ob.gravity;
+        density = ob.density;
+    }
+
+    public Object2() {
+        height = 10;
+        density = 10;
+        gravity = 10;
     }
 
 }
-public class ObjectConstructor {
-    public static void main(String[] args) {
 
+public class ObjectConstructor extends Object2 {
+    public static void main(String[] args) {
+        double density;
+        ObjectConstructor(ObjectConstructor ob) {
+            super(ob)
+        }
     }
 }
